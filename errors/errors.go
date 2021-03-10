96cc8342867d2951bfe7442dbebbe9030f0d8f60
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-03-11 00:33:03 +0900

    Add: errors

---
+++ errors/errors.go
@@ -0,0 +1,20 @@
+package errors
+
+import (
+	"errors"
+	"net/http"
+)
+
+// NotInterested indicates that the handler does not interested in the incoming events or actions.
+// When this error is returned from handlers, the processing event is falled back to another handler.
+var NotInterested = errors.New("not interested")
+
+// HttpError represents errors that can be represented as http status codes.
+// When the router receives this error, the router responds with the corresponding status code.
+type HttpError int
+
+func (e HttpError) Error() string {
+	return http.StatusText(int(e))
+}
+
+var _ error = HttpError(0)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/genkami/go-slack-event-router
+
+go 1.16
