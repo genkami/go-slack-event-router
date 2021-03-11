c6aaf48a96d3ff207dbcd67aa711fb486c3b8b45
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-03-12 00:31:06 +0900

    Test: add NewRequest

--- eventrouter_test.go
+++ eventrouter_test.go
@@ -1,6 +1,17 @@
 package eventrouter_test
 
 import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -39,4 +50,58 @@ var _ = Describe("EventRouter", func() {
 			})
 		})
 	})
+
+	Describe("WithSigningSecret", func() {
+		var (
+			r     *eventrouter.Router
+			token = "THE_TOKEN"
+		)
+		BeforeEach(func() {
+			var err error
+			r, err = eventrouter.New(eventrouter.WithSigningToken(token))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		Context("when the signature is valid", func() {
+			It("responds with 200", func() {
+				req, err := NewRequest(token, `{
+				"token": "Jhj5dZrVaK7ZwHHjRyZWjbDl",
+				"challenge": "3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P",
+				"type": "url_verification"
+			}`, nil)
+				Expect(err).NotTo(HaveOccurred())
+				w := httptest.NewRecorder()
+				r.ServeHTTP(w, req)
+				resp := w.Result()
+				body, err := ioutil.ReadAll(resp.Body)
+				fmt.Printf("body: %s\n", body)
+				Expect(resp.StatusCode).To(Equal(http.StatusOK))
+			})
+		})
+	})
 })
+
+func NewRequest(signingSecret string, body string, ts *time.Time) (*http.Request, error) {
+	hash := hmac.New(sha256.New, []byte(signingSecret))
+	var now time.Time
+	if ts == nil {
+		now = time.Now()
+	} else {
+		now = *ts
+	}
+	timestamp := strconv.FormatInt(now.Unix(), 10)
+	if _, err := hash.Write([]byte(fmt.Sprintf("v0:%s:", timestamp))); err != nil {
+		return nil, err
+	}
+	if _, err := hash.Write([]byte(body)); err != nil {
+		return nil, err
+	}
+	signature := hash.Sum(nil)
+	req, err := http.NewRequest(http.MethodPost, "http://example.com/path/to/callback", bytes.NewReader([]byte(body)))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("X-Slack-Request-Timestamp", timestamp)
+	req.Header.Set("X-Slack-Signature", "v0="+hex.EncodeToString(signature))
+	return req, nil
+}
