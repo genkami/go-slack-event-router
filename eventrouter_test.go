7a0216dbf867c885af223e3cde6b649cef7a65b7
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-03-11 22:29:47 +0900

    Test: add test for eventrouter.New

--- eventrouter_test.go
+++ eventrouter_test.go
@@ -3,10 +3,40 @@ package eventrouter_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	eventrouter "github.com/genkami/go-slack-event-router"
 )
 
 var _ = Describe("EventRouter", func() {
-	It("works", func() {
-		Expect(1 + 1).To(Equal(2))
+	Describe("New", func() {
+		Context("when neither WithSigningToken nor InsecureSkipVerification is given", func() {
+			It("returns an error", func() {
+				_, err := eventrouter.New()
+				Expect(err).To(MatchError(MatchRegexp("WithSigningToken")))
+			})
+		})
+
+		Context("when InsecureSkipVerification is given", func() {
+			It("returns a new Router", func() {
+				r, err := eventrouter.New(eventrouter.InsecureSkipVerification())
+				Expect(err).NotTo(HaveOccurred())
+				Expect(r).NotTo(BeNil())
+			})
+		})
+
+		Context("when WithSigningToken is given", func() {
+			It("returns a new Router", func() {
+				r, err := eventrouter.New(eventrouter.WithSigningToken("THE_TOKEN"))
+				Expect(err).NotTo(HaveOccurred())
+				Expect(r).NotTo(BeNil())
+			})
+		})
+
+		Context("when both WithSigningToken and InsecureSkipVerification are given", func() {
+			It("returns an error", func() {
+				_, err := eventrouter.New(eventrouter.InsecureSkipVerification(), eventrouter.WithSigningToken("THE_TOKEN"))
+				Expect(err).To(MatchError(MatchRegexp("WithSigningToken")))
+			})
+		})
 	})
 })
