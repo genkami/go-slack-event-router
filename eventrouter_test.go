c7ef700f45cd0a47de6eb0bb0d81fa2d3d3d0aeb
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-03-11 17:38:53 +0900

    Add: eventrouter_test.go

---
+++ eventrouter_test.go
@@ -0,0 +1,12 @@
+package eventrouter_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("EventRouter", func() {
+	It("works", func() {
+		Expect(1 + 1).To(Equal(2))
+	})
+})
